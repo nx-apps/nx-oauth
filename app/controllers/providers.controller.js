2e6e8693c46f925d5fa3a4bb858b6a36a0d2dea7
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-02-18 15:23:37 +0700

    add providers

---
+++ app/controllers/providers.controller.js
@@ -0,0 +1,61 @@
+exports.list = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('providers')
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.provider = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('providers')
+        .filter(req.query)
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.insert = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('providers')
+        .insert(req.body)
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.update = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('providers')
+        .get(req.body.id)
+        .update(req.body)
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.delete = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('providers')
+        .get(req.params.id)
+        .delete()
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
