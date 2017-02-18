5b0054fe69aaddb6e4c18a5e836440e247ffbd37
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-02-18 19:04:34 +0700

    get user

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,103 @@
+exports.list = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .merge((users_merge) => {
+            return {
+                providers: r.db('oauth').table('user_providers')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array'),
+                apps: r.db('oauth').table('user_apps')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array')
+            }
+        })
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.get = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .filter(req.query)
+        .merge((users_merge) => {
+            return {
+                providers: r.db('oauth').table('user_providers')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array'),
+                apps: r.db('oauth').table('user_apps')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array')
+            }
+        })
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.getById = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .get(req.params.id)
+        .merge((users_merge) => {
+            return {
+                providers: r.db('oauth').table('user_providers')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array'),
+                apps: r.db('oauth').table('user_apps')
+                    .getAll(users_merge('id'), { index: 'uid' })
+                    .coerceTo('array')
+            }
+        })
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.insert = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .insert(req.body)
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.update = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .get(req.body.id)
+        .update(req.body)
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
+exports.delete = function (req, res) {
+    var r = req.r;
+    r.db('oauth').table('users')
+        .get(req.params.id)
+        .delete()
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+}
