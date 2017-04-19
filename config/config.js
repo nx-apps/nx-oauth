804e9adf56ad759504ba733e7c43ab75d98c773d
Pongpanot Tassaneeyachumphalee <pongpanot.t@nexts-corp.com>
2017-04-19 09:47:39 +0700

    update connecting

--- config/config.js
+++ config/config.js
@@ -1,56 +1,91 @@
 var config = {
- 
+  
   development: {
-    host: "https://sys.thairiceinfo.go.th:3000",
-    appId: "d8fcffc5-d1ab-40f1-ac63-d2643d7b9464",
-    appKey: "5fc02522e60958787b8298067513c5f4b44ca78a",
+    server: {
+      port: 3000,
+      cluster:1
+    },
+    
     database: {
-      host: 'rdb.codeunbug.com',
-      port: 28015,
+      max: 10,
+      servers: [
+        {host: 'rdb.codeunbug.com', port: 28015,user:'admin',password:'next@2017'}
+      ],
+      db:'oauth'
     },
 
-    oauth: {
-      facebook: {
-        provoider: 'facebook',
-        clientId: '157316171444270',
-        clientSecret: '71c15fa338a11f411fc6432b04cad3ca',
-        callbackURL: 'https://sys.thairiceinfo.go.th:3000/oauth/facebook/callback'
-      },
-      google: {
-        provoider: 'google',
-        clientId: '464475406694-skti62k23di8uemcanuc6h6ah5nnl55a.apps.googleusercontent.com',
-        clientSecret: '24WehldQ1ZPo2hXCXmxI_FFg',
-        callbackURL: 'https://sys.thairiceinfo.go.th:3000/oauth/google/callback'
-      }
-    },
-
-    jdbc: [
-      {
-        name: "mysql",
-        driver: "com.mysql.jdbc.Driver",
-        url: "jdbc:mysql://db.codeunbug.com:3306/rmut_expert_db",
-        user: "test",
-        password: "If1C5B13eeNWvyCr"
-      },
-      {
-        name: "mssql",
-        driver: "com.microsoft.sqlserver.jdbc.SQLServerDriver",
-        url: "jdbc:sqlserver://202.44.34.86:1433;databaseName=RiceDB",
-        user: "riceuser",
-        password: "l2ice2015"
-      },
-      {
-        name: "oracle",
-        driver: "oracle.jdbc.OracleDriver",
-        url: "jdbc:oracle:thin:@25.32.200.27:1521:gl3d",
-        user: "wzeB505",
-        password: "acreporter"
-      }
-    ]
+    oauth:{
+       local:{
+         provider:'local'
+       },
+       facebook:{
+         provoider:'facebook',
+         clientId:'1438197499538604',
+         clientSecret:'d39bcc969fab3f784ec24c301535b7a4',
+         callbackURL:'http://localhost:8080/oauth/facebook/callback'
+       },
+        google:{
+         provoider:'google',
+         clientId:'464475406694-skti62k23di8uemcanuc6h6ah5nnl55a.apps.googleusercontent.com',
+         clientSecret:'24WehldQ1ZPo2hXCXmxI_FFg',
+         callbackURL:'http://localhost:8080/oauth/google/callback'
+       }
+     },
+     java:true,
+     jdbc:[
+       {
+         name:"mysql",
+         driver:"com.mysql.jdbc.Driver",
+         url:"jdbc:mysql://db.codeunbug.com:3306/rmut_expert_db",
+         user:"test",
+         password:"If1C5B13eeNWvyCr"
+       },
+        {
+         name:"mssql",
+         driver:"com.microsoft.sqlserver.jdbc.SQLServerDriver",
+         url:"jdbc:sqlserver://192.168.10.117:1433;databaseName=external",
+         user:"external",
+         password:"external@2017"
+       }
+       ,
+        {
+         name:"oracle",
+         driver:"oracle.jdbc.OracleDriver",
+         url:"jdbc:oracle:thin:@25.32.200.27:1521:gl3d",
+         user:"wzeB505",
+         password:"acreporter"
+       }
+     ]
   },
 
   production: {
-
+    server: {
+      port: 8080,
+      cluster:1
+    },
+    database: {
+      servers: [
+        {host: 'rdb.codeunbug.com', port: 28015}
+      ],
+      db:'oauth'
+    },
+    oauth:[
+       {
+         provider:'local'
+       },
+       {
+         provoider:'facebook',
+         clientId:'1438197499538604',
+         clientSecret:'d39bcc969fab3f784ec24c301535b7a4',
+         callbackURL:'http://localhost:8080/oauth/facebook/callback'
+       },
+        {
+         provoider:'google',
+         clientId:'464475406694-skti62k23di8uemcanuc6h6ah5nnl55a.apps.googleusercontent.com',
+         clientSecret:'24WehldQ1ZPo2hXCXmxI_FFg',
+         callbackURL:'http://localhost:8080/oauth/google/callback'
+       }
+    ]
   }
 
 };
