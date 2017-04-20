42a95c774ac0168e1009a5d73869dad2c747f6bd
Pongpanot Tassaneeyachumphalee <pongpanot.t@nexts-corp.com>
2017-04-20 18:08:40 +0700

    common

--- app/routes/common.routes.js
+++ app/routes/common.routes.js
@@ -1,5 +1,4 @@
 module.exports = function(app){
     var common = require('../controllers/common/common.controller');
-    app.get('/module',common.getModule);
-    //app.get('/subModule',common.getSubModule);
+    app.get('/appList',common.appList);
 }
