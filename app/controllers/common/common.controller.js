fae2cf0cc888a56946435db68916ef0313694919
chonlatee <chonlatee.s@nexts-corp.com>
2017-04-21 11:45:30 +0700

    update

--- app/controllers/common/common.controller.js
+++ app/controllers/common/common.controller.js
@@ -11,6 +11,19 @@ class common {
             res.status(500).json(err);
         })
     }
+
+    roleList(req,res){
+        var r = req.r;
+        r.table('roles')
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            res.status(500).json(err);
+        })
+    }
+
 }
 
 module.exports = new common();
