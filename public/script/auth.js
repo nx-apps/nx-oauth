df7abfb52d75c95e83bef9f5dda2f717b0c876e8
Pongpanot Tassaneeyachumphalee <pongpanot.t@nexts-corp.com>
2017-05-16 10:21:34 +0700

    edit auth

--- public/script/auth.js
+++ public/script/auth.js
@@ -2,16 +2,16 @@ import axios from './axios'
 import {baseURL} from './config'
 
 export function handleAuth(){
-    var path = window.location.pathname.split("/");
+    //var path = window.location.pathname.split("/");
  
-        if(localStorage.getItem("token")){
-            axios.post('/auth/checkToken',{token:localStorage.getItem("token")})
-            .then(res=>{
-                ReduxBehavior.dispatch({type:'AUTH_SET_USER',payload:res.data})
-            })
-            .catch(err=>{
-                localStorage.removeItem("token");
-            })
-        }
+        // if(localStorage.getItem("token")){
+        //     axios.post('/auth/checkToken',{token:localStorage.getItem("token")})
+        //     .then(res=>{
+        //         ReduxBehavior.dispatch({type:'AUTH_SET_USER',payload:res.data})
+        //     })
+        //     .catch(err=>{
+        //         localStorage.removeItem("token");
+        //     })
+        // }
 
 }
